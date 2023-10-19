9b3bc317d40ecca621e82b904234807fcc5ac27e
linxu <linxu@linxu.com>
2023-10-19 23:06:12 +0800

    first commit

---
+++ js/entity-group.js
@@ -0,0 +1,193 @@
+class EntityGroup{
+    constructor(entity,entity_start_x,entity_start_y,entity_stop_x,entity_stop_y){
+        this.start_x=entity_start_x;
+        this.start_y=entity_start_y;
+        this.stop_x=entity_stop_x;
+        this.stop_y=entity_stop_y;
+        this.entities=[entity];
+    }
+    push_entity(entity,entity_stop_x,entity_stop_y){
+        this.stop_x=entity_stop_x;
+        this.stop_y=entity_stop_y;
+        
+        this.entities.push(entity);
+    }
+    unshift_entity(entity,entity_start_x,entity_start_y){
+        this.start_x=entity_start_x;
+        this.start_y=entity_start_y;
+        this.entities.unshift(entity);
+    }
+
+    // 合并两个 entity_group
+    push_group(another_group){
+        this.entities=this.entities.concat(another_group.entities)
+        this.stop_x=another_group.stop_x;
+        this.stop_y=another_group.stop_y;
+    }
+
+    unshift_group(another_group){
+        this.entities=[].concat(another_group.entities,this.entities);
+        this.start_x=another_group.start_x;
+        this.start_y=another_group.start_y;
+    }
+
+    push_reversed_group(another_group){
+        var that_entities=[];
+        for (var entity of another_group.entities){
+            that_entities.unshift(reverseEntity(entity));
+        }
+        this.entities=this.entities.concat(that_entities)
+        this.stop_x=another_group.start_x;
+        this.stop_y=another_group.start_y;
+    }
+
+    unshift_reversed_group(another_group){
+        var that_entities=[];
+        for (var entity of another_group.entities){
+            that_entities.unshift(reverseEntity(entity));
+        }
+        this.entities=[].concat(that_entities,this.entities);
+        this.start_x=another_group.stop_x;
+        this.start_y=another_group.stop_y;
+    }
+}
+
+function valueEqual(a,b){
+    return Math.abs(a-b)< 10e-5;
+}
+
+
+function reverseEntity(entity){
+    // 实际需要，目前仅仅支持 ARC和 LINE
+    let temp;
+    if (entity.type=="ARC"){
+        temp=entity.startAngle;
+        entity.startAngle=entity.endAngle;
+        entity.endAngle=temp;
+        entity.rotateDir=!entity.rotateDir;
+
+    }else if (entity.type=="LINE"){
+        temp=entity.vertices[0]["x"];
+        entity.vertices[0]["x"]=entity.vertices[1]["x"];
+        entity.vertices[1]["x"]=temp;
+        temp=entity.vertices[0]["y"];
+        entity.vertices[0]["y"]=entity.vertices[1]["y"];
+        entity.vertices[1]["y"]=temp;
+    }else{
+        throw("Unsupport Entity Type: ",entity.type);
+    }
+    return entity;
+}
+
+export function getEntityGroups(dxf){
+    var temp_entity_groups=[];
+    dxf.entities.forEach(entity => {
+        let entity_start_x,entity_start_y,entity_stop_x,entity_stop_y;
+        
+
+        // 鱼咬尾
+        if (entity.type=="ARC"){
+            let center_x,center_y,radius,start_angle,stop_angle;
+            center_x=entity.center["x"];
+            center_y=entity.center["y"];
+            radius=entity.radius;
+            start_angle=entity.startAngle;
+            stop_angle=entity.endAngle;
+            // 计算 entity_start_x,entity_start_y,entity_stop_x,entity_stop_y
+            entity_start_x=center_x+radius*Math.cos(start_angle);
+            entity_start_y=center_y+radius*Math.sin(start_angle);
+            entity_stop_x=center_x+radius*Math.cos(stop_angle);
+            entity_stop_y=center_y+radius*Math.sin(stop_angle);
+            entity.rotateDir=false;
+
+        }else if (entity.type=="LINE"){
+            entity_start_x=entity.vertices[0]["x"];
+            entity_start_y=entity.vertices[0]["y"];
+            entity_stop_x=entity.vertices[1]["x"];
+            entity_stop_y=entity.vertices[1]["y"];
+        }else{
+            throw("Unsupport Entity Type: ",entity.type);
+        }
+
+        // 默认 entity 无家可归
+        var homeless = true;
+        // 进入 丑小鸭找妈妈 环节
+        for (var i=0;i <temp_entity_groups.length;i++){
+            var group=temp_entity_groups[i]
+            if (valueEqual(group.start_x,entity_stop_x) &&valueEqual(group.start_y,entity_stop_y)){
+                // group 头碰 entity 尾
+                // start -> stop
+                // [<-<-<-][<-]
+                group.unshift_entity(entity,entity_start_x,entity_start_y);
+                homeless = false;
+                break;
+            }else if (valueEqual(group.stop_x,entity_start_x) && valueEqual(group.stop_y,entity_start_y)){
+                // 尾碰头
+                //[<-][<-<-<-]
+                group.push_entity(entity,entity_stop_x,entity_stop_y);
+                homeless = false;
+                break;
+            }else if (valueEqual(group.start_x,entity_start_x) &&valueEqual(group.start_y,entity_start_y)){
+                // group 头碰头
+                //[<-<-<-][->]
+                group.unshift_entity(reverseEntity(entity),entity_stop_x,entity_stop_y);
+                homeless = false;
+                break;
+            }else if (valueEqual(group.stop_x,entity_stop_x) && valueEqual(group.stop_y,entity_stop_y)){
+                // 尾碰尾
+                //[->][<-<-<-]
+                group.push_entity(reverseEntity(entity),entity_start_x,entity_start_y);
+                homeless = false;
+                break;
+            }
+        }
+
+        // 自立门户
+        if (homeless){
+            temp_entity_groups.push(new EntityGroup(entity,entity_start_x,entity_start_y,entity_stop_x,entity_stop_y));
+            return;
+        }
+
+    });
+
+    var merged_entity_groups=[];
+    temp_entity_groups.forEach(group => {
+        // 默认 group 无家可归
+        var homeless = true;
+        merged_entity_groups.forEach(merged_group =>{
+            if (valueEqual(merged_group.stop_x,group.start_x) && valueEqual(merged_group.stop_y,group.start_y)){
+                // 头碰尾
+                // start -> stop
+                // m: merged_group g: group
+                // [<-<-g<-<-][<-<-m<-<-]
+                merged_group.push_group(group);
+                homeless = false;
+            }else if (valueEqual(merged_group.start_x,group.stop_x) && valueEqual(merged_group.start_y,group.stop_y)){
+                // 尾碰头
+                // m: merged_group g: group
+                // [<-<-m<-<-][<-<-g<-<-]
+                merged_group.unshift_group(group);
+                homeless = false;
+            }else if (valueEqual(merged_group.stop_x,group.stop_x) && valueEqual(merged_group.stop_y,group.stop_y)){
+                // 尾碰尾
+                // m: merged_group g: group
+                // [->->m->->][<-<-g<-<-]
+                merged_group.push_reversed_group(group);
+                homeless = false;
+            }else if (valueEqual(merged_group.start_x,group.start_x) && valueEqual(merged_group.start_y,group.start_y)){
+                
+                // 头碰头
+                // m: merged_group g: group
+                // [<-<-g<-<-][->->m->->]
+                merged_group.unshift_reversed_group(group);
+                homeless = false;
+            }
+        })
+        // 开宗立派
+        if (homeless){
+            merged_entity_groups.push(group);
+        }
+        
+    })
+    return merged_entity_groups;
+}
---
+++ js/main.js
@@ -0,0 +1,277 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
+import { FontLoader } from 'three/addons/loaders/FontLoader.js';
+import { GUI } from "three/addons/libs/lil-gui.module.min.js";
+
+import { DxfParser } from "dxf-parser";
+
+import { getEntityGroups } from "./entity-group";
+
+// 添加按钮点击事件的监听器
+const importButton = document.getElementById("importButton");
+importButton.addEventListener("click", function () {
+  var fileInput = document.createElement("input");
+  fileInput.type = "file";
+  fileInput.accept = ".dxf"; // 只接受文本文件，可以根据需要更改
+  fileInput.addEventListener("change", importFile);
+  fileInput.click();
+});
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+scene.background = new THREE.Color("white");
+
+const fov = 75;
+const aspect = 2; // the canvas default
+const near = 0.1;
+const far = 1000;
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+
+camera.position.set(60, 20, 100);
+camera.lookAt(0, 0, 0);
+
+const canvas = renderer.domElement;
+const controls = new OrbitControls(camera, canvas);
+
+controls.enablePan = true; // 是否开启右键拖拽
+controls.dampingFactor = 0.5; // 动态阻尼系数 就是鼠标拖拽旋转灵敏度，阻尼越小越灵敏
+
+controls.enableRotate = true;
+controls.autoRotate = true; // 是否自动旋转
+controls.autoRotateSpeed = 8.0;
+controls.rotateSpeed = 2.0;
+
+controls.target.set(0, 5, 0);
+controls.update();
+
+// 坐标系 平面
+// {
+//     //系统坐标系绘制
+//     const axesHelper = new THREE.AxesHelper(12);
+//     scene.add(axesHelper);
+
+//     const planeSize = 1000;
+
+//     const loader = new THREE.TextureLoader();
+//     const texture = loader.load( 'https://threejs.org/manual/examples/resources/images/checker.png' );
+//     texture.wrapS = THREE.RepeatWrapping;
+//     texture.wrapT = THREE.RepeatWrapping;
+//     texture.magFilter = THREE.NearestFilter;
+//     texture.colorSpace = THREE.SRGBColorSpace;
+//     const repeats = planeSize / 2;
+//     texture.repeat.set( repeats, repeats );
+
+//     const planeGeo = new THREE.PlaneGeometry( planeSize, planeSize );
+//     const planeMat = new THREE.MeshPhongMaterial( {
+//         map: texture,
+//         side: THREE.DoubleSide,
+//     } );
+//     const mesh = new THREE.Mesh( planeGeo, planeMat );
+//     mesh.rotation.x = Math.PI * - .5;
+//     scene.add( mesh );
+// }
+
+// 控制器
+{
+  class ColorGUIHelper {
+    constructor(object, prop) {
+      this.object = object;
+      this.prop = prop;
+    }
+    get value() {
+      return `#${this.object[this.prop].getHexString()}`;
+    }
+    set value(hexString) {
+      this.object[this.prop].set(hexString);
+    }
+  }
+  class DegRadHelper {
+    constructor(obj, prop) {
+      this.obj = obj;
+      this.prop = prop;
+    }
+    get value() {
+      return THREE.MathUtils.radToDeg(this.obj[this.prop]);
+    }
+    set value(v) {
+      this.obj[this.prop] = THREE.MathUtils.degToRad(v);
+    }
+  }
+  function makeXYZGUI(gui, vector3, name, onChangeFn) {
+    const folder = gui.addFolder(name);
+    folder.add(vector3, "x", -100, 100).onChange(onChangeFn);
+    folder.add(vector3, "y", 0, 1000).onChange(onChangeFn);
+    folder.add(vector3, "z", -100, 100).onChange(onChangeFn);
+    folder.open();
+  }
+
+  const color = 0xffffff;
+  const intensity = 10000;
+  const light = new THREE.SpotLight(color, intensity);
+  light.position.set(0, 100, 40);
+  light.target.position.set(0, 0, 0);
+  scene.add(light);
+  scene.add(light.target);
+
+  function updateLight() {
+    light.target.updateMatrixWorld();
+  }
+
+  updateLight();
+
+  const gui = new GUI();
+  gui.addColor(new ColorGUIHelper(light, "color"), "value").name("color");
+
+  gui.add(light, "intensity", 0, 10000, 1);
+  gui.add(light, "distance", 0, 1000).onChange(updateLight);
+  gui
+    .add(new DegRadHelper(light, "angle"), "value", 0, 90)
+    .name("angle")
+    .onChange(updateLight);
+  gui.add(light, "penumbra", 0, 1, 0.01);
+
+  makeXYZGUI(gui, light.position, "position", updateLight);
+  makeXYZGUI(gui, light.target.position, "target", updateLight);
+}
+var obj_list = [];
+
+const reader = new FileReader();
+var dxf;
+const parser = new DxfParser();
+
+
+// 开场
+{
+  const textLoader = new FontLoader();
+  //导入字体
+  textLoader.load(
+    `./fonts/FuturaLT-Heavy.typeface.json`,
+    font => {
+      const textGeometry = new TextGeometry("HI MOTOR", {
+        font,
+        size: 10,
+        height: 10,
+        curveSegments: 12,
+        bevelEnabled: true,
+        bevelThickness: 0.03,
+        bevelSize: 0.02,
+        bevelOffset: 0,
+        bevelSegments: 5,
+      })
+    var material = new THREE.MeshPhongMaterial({
+      color: 0xffffff * Math.random(),
+      transparent: true,
+      opacity: 0.8,
+    });
+    var model = new THREE.Mesh(textGeometry, material)
+    obj_list.push(model);
+    scene.add(model);
+  })
+}
+render();
+
+// const defaultURL = './models/fluid.dxf';
+// fetch(defaultURL)
+//     .then(response => response.text())
+//     .then(content => {
+//         updateModel(content)
+//     })
+//     .catch(error => {
+//         console.error('加载默认URL时发生错误：', error);
+//     });
+
+// 解析 dxf
+function updateModel(content) {
+  try {
+    dxf = parser.parse(content);
+  } catch (err) {
+    return console.error(err.stack);
+  }
+
+  var entity_groups = getEntityGroups(dxf);
+
+  obj_list.forEach((obj) => scene.remove(obj));
+
+  var options = {
+    amount: 5,
+    bevelThickness: 10,
+    bevelSize: 0,
+    bevelSegments: 0,
+    bevelEnabled: true,
+    depth: 5,
+    depthWrite: false,
+    // curveSegments: 1,
+    // steps: 5,
+  };
+  var z_delta = 0.05;
+  entity_groups.forEach((group) => {
+    var shape = new THREE.Shape();
+    group.entities.forEach((entity) => {
+      if (entity.type == "LINE") {
+        shape.moveTo(entity.vertices[0]["x"], entity.vertices[0]["y"]);
+        shape.lineTo(entity.vertices[1]["x"], entity.vertices[1]["y"]);
+      } else if (entity.type == "ARC") {
+        shape.absarc(
+          entity.center["x"],
+          entity.center["y"],
+          entity.radius,
+          entity.startAngle,
+          entity.endAngle,
+          entity.rotateDir
+        );
+      }
+    });
+    // var geometry = new THREE.ShapeGeometry(shape);
+    var geometry = new THREE.ExtrudeGeometry(shape, options);
+    var material = new THREE.MeshPhongMaterial({
+      color: 0xffffff * Math.random(),
+      transparent: true,
+      opacity: 0.6,
+    });
+    var model = new THREE.Mesh(geometry, material);
+    // var model = new THREE.LineSegments( geometry, material );
+    // var model = new THREE.Points( geometry, material );
+    model.position.z = z_delta;
+    z_delta += z_delta;
+    obj_list.push(model);
+    scene.add(model);
+  });
+  render();
+}
+
+function resizeRendererToDisplaySize(renderer) {
+  const width = canvas.clientWidth;
+  const height = canvas.clientHeight;
+  const needResize = canvas.width !== width || canvas.height !== height;
+  if (needResize) {
+    renderer.setSize(width, height, false);
+  }
+  return needResize;
+}
+
+function render() {
+  if (resizeRendererToDisplaySize(renderer)) {
+    camera.aspect = canvas.clientWidth / canvas.clientHeight;
+    camera.updateProjectionMatrix();
+  }
+  controls.update();
+  renderer.render(scene, camera);
+
+  requestAnimationFrame(render);
+}
+
+// 导入文件的函数
+function importFile(event) {
+  const file = event.target.files[0];
+  reader.onload = function (e) {
+    const contents = e.target.result;
+    // 在这里处理导入文件的逻辑
+    updateModel(contents);
+  };
+
+  reader.readAsText(file);
+}
